a3090caaa9601986be7e6588770037826196f3d7
unknown <zlovred12@gmail.com>
2022-06-15 18:33:49 +0300

    'lesson5'

--- src/ru/imbabot/java2/lesson5/Main.java
+++ src/ru/imbabot/java2/lesson5/Main.java
@@ -8,8 +8,8 @@ public class Main {
     private static final int H = SIZE/2;
     private static final float[] arr = new float[SIZE];
 
-    private static float[] first = new float[H];
-    private static float[] second = new float[H];
+    private static float[] first = new float[SIZE];
+    private static float[] second = new float[SIZE];
 
 
     private static void method1(){
@@ -40,22 +40,21 @@ public class Main {
 
     }
 
-    private static void method2(){
-
-        for (int i = 0; i < arr.length; i++) {
+    private static String method2(){
+        for (int i = 0; i < arr.length ; i++) {
             arr[i] = 1;
         }
 
         long a = System.currentTimeMillis();
-
-
         System.arraycopy(arr, 0, first, 0, H);
-        System.arraycopy(arr, H, second, 0, H);
+        System.arraycopy(arr, H, second, H, H);
 
-        Thread t = new Thread(new ArrThread(first,0));
+
+        Thread t = new Thread(new ArrThread(first, 0));
         Thread t2 = new Thread(new ArrThread(second, H));
         t.start();
         t2.start();
+
         try {
             t.join();
             t2.join();
@@ -65,20 +64,21 @@ public class Main {
 
 
         System.arraycopy(first, 0, arr, 0, H);
-        System.arraycopy(second, 0, arr, H, H);
+        System.arraycopy(second, H, arr, H, H);
 
+        long time = System.currentTimeMillis() - a;
 
-        System.out.println(System.currentTimeMillis() - a);
-
-        float result = 0;
-
+        float second = 0;
         for (int i = 0; i <arr.length ; i++) {
-            result += arr[i];
+            second += arr[i];
         }
 
-        System.out.println(result);
 
+        System.out.println(second);
+
+        return time + " " + second;
     }
+
 
 
     public static void main(String[] args) {
